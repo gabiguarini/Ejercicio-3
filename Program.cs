bac7d78130987f302f72e862df430d63cd997c49
Gabi Guarini <gabibea97@gmail.com>
2020-01-31 20:32:47 -0300

    oops

--- Program.cs
+++ Program.cs
@@ -7,53 +7,29 @@ namespace Ejercicio_3
        
         public static void Main(string[] args)
         {
-             Program ob = new Program();
-
-             int nro;
-             bool result;
-
-            string happy = " es múltiplo de 3 pero no de 5, genial!";
-            string sad = " boo! al parecer no es lo que buscamos :(";
-
             Console.WriteLine("Ingrese cualquier nro :)");
-            nro = int.Parse(Console.ReadLine());
-
-            result = ob.esMultiplo(nro);
-            
+            int nro = int.Parse(Console.ReadLine());
             Console.WriteLine();
-            Console.WriteLine(nro + (result?happy:sad));
             listado(nro);
 
         }
 
         public static void listado(int nro)
         {
-
-            for (int i = 1; i < nro | i == nro & i % 5 != 0; i++)
-            {
-                Console.WriteLine(i * 3 + " es múltiplo de 3");
-            }
-
-
-        }
-
-        public bool esMultiplo(int nro)
-        {
             
-        
-            if (nro % 3 == 0 & nro % 5 != 0)
-            {   
+            int veces = nro+1;
+            for (int i = 1; i < veces;)
+            {
+                if(i % 5 != 0)
+                {
+                    Console.WriteLine(i * 3 + " es múltiplo de 3");
+                }
                 
-                return true;
-                
+                 i++;
             }
 
-           
-            return false;  
-           
-        }
 
-        
+        }
 
     }
 }
